98b59f3e102d1aaca011bcbe278122dd0f3b3263
Vera Simon <vera.simon01@gmail.com>
2019-02-13 16:49:06 -0800

    boilerplate Added some various boilerplate

--- src/player.py
+++ src/player.py
@@ -1,2 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    def __init__(self, name, x, y):
+        self.items = []
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def move(self):
+        pass
+
+    def get_items(self):
+        pass
+
+    def set_items(self):
+        pass
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room():
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+        self.items = []  # list of items
+        self.n_to = {}
+        self.e_to = {}
+        self.s_to = {}
+        self.w_to = {}
